94ae74944f90bf3bf005f0ceaa51a9d493680505
Leon-Paul Hart <leon.paul.hart@gmail.com>
2017-04-04 15:02:27 +0100

    2 tests passing

--- shopping_basket.js
+++ shopping_basket.js
@@ -2,4 +2,12 @@ var ShoppingBasket = function(){
   this.contents = [];
 }
 
+ShoppingBasket.prototype = {
+  addToBasket: function(item) {
+    this.contents.push(item)
+
+  }
+}
+
+
 module.exports = ShoppingBasket;
--- specs/shopping_basket_spec.js
+++ specs/shopping_basket_spec.js
@@ -1,20 +1,29 @@
 var assert = require("assert");
-var ShoppingBasket = require("../shopping_basket.js")
+var Item = require("../item.js");
+var ShoppingBasket = require("../shopping_basket.js");
 
 describe("Shopping Basket", function(){
+
   var shoppingBasket;
+  var item1;
+  var item2;
+  var item3;
 
   beforeEach(function(){
     shoppingBasket = new ShoppingBasket();
+    item1 = new Item("Milk" , 100 , false);
+    item2 = new Item("Beer", 500 , false );
+    item3 = new Item("Cookies" , 200, true );
+
   })
 
-  it("should be empty at start", function(){
+  it("Basket should be empty at start", function(){
     assert.strictEqual(0,shoppingBasket.contents.length)
   })
 
-  it("can add items to basket", function(){
-    shoppingBasket.addToBasket();
+  it("Item can add items to basket", function(){
+    shoppingBasket.addToBasket(item1);
     assert.strictEqual(1,shoppingBasket.contents.length)
   })
-  
+
 })
